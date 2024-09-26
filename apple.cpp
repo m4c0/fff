88b22a2fed02a0c5172d7ca45923880b999973bc
Eduardo Costa <m4c0@users.noreply.github.com>
2024-09-26 13:18:17 +0100

    apple implementation

--- apple.cpp
+++ apple.cpp
@@ -1,3 +1,6 @@
+module;
+#include <CoreFoundation/CoreFoundation.h>
+
 module fff;
 import silog;
 
@@ -5,12 +8,28 @@ using namespace fff;
 
 namespace {
   class cf_timer : public timer_t {
+    CFRunLoopTimerRef m_ref;
+
   public:
-    cf_timer() {}
-    virtual ~cf_timer() {}
+    explicit cf_timer(CFRunLoopTimerRef r) : m_ref { r } {}
+    virtual ~cf_timer() { CFRelease(m_ref); }
   };
-}
+} // namespace
 
 timer_t * timer::create(unsigned ms) {
-  return new cf_timer {};
+  CFAbsoluteTime secs = ms / 1000.0f;
+
+  CFRunLoopTimerContext ctx { .info = this };
+
+  constexpr const auto timer_cb = [](CFRunLoopTimerRef ref, void * info) {
+    auto self = static_cast<timer *>(info);
+    self->m_fn();
+  };
+
+  auto fire_date = CFAbsoluteTimeGetCurrent() + secs;
+
+  auto t = CFRunLoopTimerCreate(nullptr, fire_date, secs, 0, 0, timer_cb, &ctx);
+  CFRunLoopAddTimer(CFRunLoopGetMain(), t, kCFRunLoopCommonModes);
+  // Setup notifications for enter bg
+  return new cf_timer { t };
 }
